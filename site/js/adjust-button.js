580c1f0e0755569ff7caedcf370b560e8e0cfa23
Emily Danielson <emily@griptape.ai>
2024-07-19 14:25:11 -0700

    removing site/ from repo

--- site/js/adjust-button.js
+++
@@ -1,7 +0,0 @@
-// raises the readthedocs version button
-document.addEventListener("DOMContentLoaded", function () {
-  var versionButton = document.querySelector(".rst-current-version");
-  if (versionButton) {
-    versionButton.style.bottom = "60px"; // Adjust as necessary
-  }
-});
